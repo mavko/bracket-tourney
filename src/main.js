72597db18c815ee312f26f55ffb2f9578c5aebac
Sebastian Selling <sebastian.selling@outlook.com>
2021-09-03 00:26:10 +0200

    App to app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import app from './app.vue'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
@@ -8,5 +8,5 @@ Vue.config.productionTip = false
 
 new Vue({
 	vuetify,
-	render: (h) => h(App),
+	render: (h) => h(app),
 }).$mount('#app')
