fd606816bc1c9c858f28de2779f14d7edf2cec7b
Sebastian Selling <sebastian.selling@outlook.com>
2021-09-03 00:25:37 +0200

    starter group comps

---
+++ src/components/team-groups/starter-groups/starter-group-four.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<v-row no-gutters justify="space-between" class="my-5">
+			<v-col cols="12" md="3" lg="3" class="flex-grow-0 flex-shrink-0">
+				<v-alert
+					light
+					min-width="300"
+					class="text-body-1 font-weight-bold black--text mb-2 overflow-hidden"
+					rounded="lg"
+					elevation="12"
+				>
+					<!-- 
+						Edit team logo
+					-->
+					<v-avatar left color="white" size="30" class="mr-1">
+						<img src="@/assets/logos/dotaunderload.svg" alt="" />
+					</v-avatar>
+					<!-- 
+						Edit team name and score
+					-->
+					Natus Vincere
+					<div class="text-right team-score">2</div>
+					<!-- 
+						Edit team win/lose colors by
+						css class __loser or __winner
+					-->
+					<div class="v-alert__border__winner" />
+				</v-alert>
+				<v-alert
+					light
+					min-width="300"
+					class="text-body-1 font-weight-bold black--text mb-2 overflow-hidden"
+					rounded="lg"
+					elevation="12"
+				>
+					<!-- 
+						Edit team logo
+					-->
+					<v-avatar left color="white" size="30" class="mr-1">
+						<img src="@/assets/logos/hearthstone.svg" alt="" />
+					</v-avatar>
+					<!-- 
+						Edit team name and score
+					-->
+					BIG
+					<div class="text-right team-score">0</div>
+					<!-- 
+						Edit team win/lose colors by
+						css class __loser or __winner
+					-->
+					<div class="v-alert__border__loser" />
+				</v-alert>
+			</v-col>
+		</v-row>
+	</div>
+</template>
+<script>
+export default {
+	name: 'starterGroupFour',
+}
+</script>
---
+++ src/components/team-groups/starter-groups/starter-group-one.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<v-row no-gutters justify="space-between">
+			<v-col cols="12" md="3" lg="3" class="flex-grow-0 flex-shrink-0">
+				<v-alert
+					light
+					min-width="300"
+					class="text-body-1 font-weight-bold black--text mb-2 overflow-hidden"
+					rounded="lg"
+					elevation="12"
+				>
+					<!-- 
+						Edit team logo
+					-->
+					<v-avatar left color="white" size="30" class="mr-1">
+						<img src="@/assets/logos/100-thieves-logo.svg" alt="" />
+					</v-avatar>
+					<!-- 
+						Edit team name and score
+					-->
+					100 Thieves
+					<div class="text-right team-score">2</div>
+					<!-- 
+						Edit team win/lose colors by
+						css class __loser or __winner
+					-->
+					<div class="v-alert__border__winner"></div>
+				</v-alert>
+				<v-alert
+					light
+					min-width="300"
+					class="text-body-1 font-weight-bold black--text mb-2 overflow-hidden"
+					rounded="lg"
+					elevation="12"
+				>
+					<!-- 
+						Edit team logo
+					-->
+					<v-avatar left color="white" size="30" class="mr-1">
+						<img src="@/assets/logos/cloud-data.svg" alt="" />
+					</v-avatar>
+					<!-- 
+						Edit team name and score
+					-->
+					Evil Geniuses
+					<div class="text-right team-score">0</div>
+					<!-- 
+						Edit team win/lose colors by
+						css class __loser or __winner
+					-->
+					<div class="v-alert__border__loser"></div>
+				</v-alert>
+			</v-col>
+		</v-row>
+	</div>
+</template>
+<script>
+export default {
+	name: 'starterGroupOne',
+}
+</script>
