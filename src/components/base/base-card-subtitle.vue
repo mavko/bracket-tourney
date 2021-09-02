9ac051b4bd48545bca0692606354ed970846bea0
Sebastian Selling <sebastian.selling@outlook.com>
2021-09-03 00:28:10 +0200

    base layout sub comp

---
+++ src/components/base/base-card-subtitle.vue
@@ -0,0 +1,26 @@
+<template>
+	<!-- 
+		Page Subtitle
+    Edit game, nr of teams, tourney type
+	-->
+<div>
+  <v-card-subtitle>
+    <v-row dense class="text-h6">
+      <v-col cols="auto">
+        Valorant
+      </v-col>
+      <v-col cols="auto">
+        8/8 Teams (5v5)
+      </v-col>
+      <v-col cols="auto">
+        Groups then Single Elimination
+      </v-col>
+    </v-row>
+  </v-card-subtitle>
+</div>
+</template>
+<script>
+export default {
+	name: 'baseCardTitle',
+}
+</script>
