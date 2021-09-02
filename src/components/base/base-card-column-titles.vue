366f58160abfe787f65ce03b9228ed27763009c0
Sebastian Selling <sebastian.selling@outlook.com>
2021-09-03 00:27:57 +0200

    base layout title comp

---
+++ src/components/base/base-card-column-titles.vue
@@ -0,0 +1,20 @@
+<template>
+	<!-- 
+		Page Column Titles
+    Edit Tourney Group titles
+	-->
+<div>
+  <v-divider></v-divider>
+  <v-row class="my-2 text-body-1" justify="space-between">
+    <v-col cols="auto">Semi-finals</v-col>
+    <v-col cols="auto">Finals</v-col>
+    <v-col cols="auto">Grand Final</v-col>
+  </v-row>
+  <v-divider class="mb-5"></v-divider>
+</div>
+</template>
+<script>
+export default {
+	name: 'baseCardColumnTitles',
+}
+</script>
