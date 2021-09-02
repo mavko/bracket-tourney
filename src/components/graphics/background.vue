f7ae944051758beb937e6d6949c87784f280bf5a
Sebastian Selling <sebastian.selling@outlook.com>
2021-09-03 00:26:30 +0200

    gfx comps

---
+++ src/components/graphics/background.vue
@@ -0,0 +1,28 @@
+<template>
+	<!-- 
+		Switchable per tournament theme
+	-->
+	<div>
+    <img src="@/assets/bgs/valo.png" class="main-bg" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'background'
+}
+</script>
+<style scoped>
+/* Valo background positioning
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+.main-bg {
+	position: absolute;
+	z-index: 0;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: auto;
+	image-rendering: optimizespeed;
+	background-position: top;
+	background-size: cover;
+}
+</style>
---
+++ src/components/graphics/connectors.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="connector">
+		<div class="merger"></div>
+		<div class="line"></div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'connectors',
+}
+</script>
+<style scoped>
+/* Custom css lines to connect the team badges visually
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+.connector {
+	left: 30%;
+	top: 33%;
+	position: absolute;
+	border-radius: 20px;
+}
+.line,
+.connector .merger {
+	box-sizing: border-box;
+	width: 16.4rem;
+	display: inline-block;
+	vertical-align: middle;
+}
+.connector .line {
+	border-top: 2px solid #7c8abb;
+	border-right: 2px solid #7c8abb;
+	border-bottom: 2px solid #7c8abb;
+	height: 12rem;
+	width: 46.5rem;
+	border-top-right-radius: 20px;
+	border-bottom-right-radius: 20px;
+}
+.connector .merger {
+	position: relative;
+	height: 41rem;
+	border-radius: 20px;
+}
+.connector .merger:before,
+.connector .merger:after {
+	content: '';
+	display: block;
+	box-sizing: border-box;
+	width: 100%;
+	height: 50%;
+	border: 0 solid;
+	border-color: #7c8abb;
+}
+.connector .merger:before {
+	border-right-width: 2px;
+	border-top-width: 2px;
+	border-top-right-radius: 20px;
+}
+.connector .merger:after {
+	border-right-width: 2px;
+	border-bottom-width: 2px;
+	border-bottom-right-radius: 20px;
+}
+/* Minor media queries for desktop and laptop
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+@media screen and (max-width: 1300px) {
+	.line {
+		width: 29.8rem !important;
+	}
+	.merger {
+		width: 11.4rem !important;
+	}
+}
+@media screen and (max-width: 1450px) {
+	.line {
+		width: 32.4rem !important;
+	}
+	.merger {
+		width: 13rem !important;
+	}
+}
+</style>
